3986d8089f9ea7b4a9ca29fcc56fb4bd0dfb3e06
cxb1988 <283817727@qq.com>
2021-01-18 10:16:47 +0800

    es

--- Crawler/biquge/bookList.go
+++ Crawler/biquge/bookList.go
@@ -11,29 +11,28 @@ import (
 
 var bookListRe = regexp.MustCompile(`<dd><a href='([a-zA-Z0-9/.]+)'[^>]*>([^<]+)</a></dd>`)
 
-var imgRe = regexp.MustCompile(`<img alt=".+" src="(http://www.xbiquge.la/files/article/[a-zA-Z0-9./]+)" [^>]*>`)
+var imgRe = regexp.MustCompile(`<img alt="(.+)" src="(http://www.xbiquge.la/files/article/[a-zA-Z0-9./]+)" [^>]*>`)
 var upTimeRe = regexp.MustCompile(`<p>最后更新：([^<]+)</p>`)
 var nearSectionRe = regexp.MustCompile(`<p>最新章节：<a href="http://www.xbiquge.la/[a-zA-Z0-9./]+">([^<]+)</a></p>`)
 var autoRe = regexp.MustCompile(`<p>[\s\S]+?者：([\s\S]+?)</p>`)
 var descRe = regexp.MustCompile(`<div id="intro">([\s\S]+?)</div>`)
 
-
 // 小书列表页
-func ParseBookList(contents []byte ,bookName string) engine.ParseResult {
+func ParseBookList(contents []byte, uList string) engine.ParseResult {
 	result := engine.ParseResult{}
-	if len(contents) < 1{
-		fmt.Println("空的内容，小书:",bookName)
+	if len(contents) < 1 {
+		fmt.Println("空的内容，小书:")
 		return result
 	}
-	matches := bookListRe.FindAllSubmatch(contents,-1)
+	//matches := bookListRe.FindAllSubmatch(contents,-1)
 	item := make(map[string]interface{})
 	bookImg := imgRe.FindSubmatch(contents)
- 	item["book_thumb_img"] = string(bookImg[1])
+	item["book_thumb_img"] = string(bookImg[2])
 
 	CreatedAt := upTimeRe.FindSubmatch(contents)
 	item["updated_at"] = string(CreatedAt[1])
 
-	item["book_title"] = bookName
+	item["book_title"] = string(bookImg[1])
 	nearSection := nearSectionRe.FindSubmatch(contents)
 	item["last_article_title"] = string(nearSection[1])
 	isFinish := 0
@@ -52,23 +51,27 @@ func ParseBookList(contents []byte ,bookName string) engine.ParseResult {
 
 	bookAuto := autoRe.FindSubmatch(contents)
 	item["book_auth"] = string(bookAuto[1])
-	fmt.Println(item)
 	book := Services.NewBookService()
 
 	code, msg, bookId := book.AddBook(item)
 	if code == Helpers.SUCCESS {
-		for _, m := range matches{
-			bookName := string(m[2])
-			result.Request = append(result.Request,engine.Request{
-				Url:      "http://www.xbiquge.la"+string(m[1]),
-				ParseFun: func(bytes []byte) engine.ParseResult {
-					return ClimbDetail(bytes,bookId,bookName)
-				},
-			})
-			result.Items = append(result.Items,bookName)
-		}
+		result.Items = append(result.Items, engine.Item{
+			Url:     uList,
+			Type:    "玄幻",
+			Id:      string(bookId),
+			Payload: item,
+		})
+		//for _, m := range matches{
+		//	bookName := string(m[2])
+		//	result.Request = append(result.Request,engine.Request{
+		//		Url:      "http://www.xbiquge.la"+string(m[1]),
+		//		ParseFun: func(bytes []byte) engine.ParseResult {
+		//			return ClimbDetail(bytes,bookId,bookName)
+		//		},
+		//	})
+		//}
 	} else {
-		fmt.Println("文章添加失败:",msg)
+		fmt.Println("文章添加失败:", msg)
 	}
 	return result
 }
